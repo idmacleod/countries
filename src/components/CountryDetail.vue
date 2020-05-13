8d6c83149c76a5e1b99275e91edc1d4a2b849115
idmacleod <idmacleod@gmail.com>
2020-05-13 19:00:30 +0100

    Capital, population, languages and flag added to CountryDetail

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,8 +1,16 @@
 <template>
     <div v-if="country">
         <h3>{{ country.name }}</h3>
-        <p>Capital: {{country.capital}}</p>
-        <p>Population: {{country.population}}</p>
+        <ul>
+            <li>Capital: {{country.capital}}</li>
+            <li>Population: {{country.population}}</li>
+            <li>Languages Spoken:
+            <ul>
+                <li v-for="(language, index) in country.languages" :key="index">{{language.name}}</li>
+            </ul>
+            </li>
+        </ul>
+        <img :src="country.flag" alt="flag">
     </div>
 </template>
 
