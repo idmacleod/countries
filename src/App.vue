0687b0a38983d09e956a07a80eeda81dfa2dfbe1
idmacleod <idmacleod@gmail.com>
2020-05-13 15:47:17 +0100

    Created App.vue, and other component files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Countries</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data: function () {
+    return {
+      countries: [],
+      selectedCountry: null
+    }
+  },
+  mounted: function () {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(countries => this.countries = countries)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
